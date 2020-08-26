27dc09696edf672175f8c0025786cf26a208b0e2
Keerthana531 <70246947+Keerthana531@users.noreply.github.com>
2020-08-26 18:48:56 +0530

    Add files via upload

---
+++ Functional_Testing/src/test/java/com/yoloj/PageObjects/LoginPageObject.java
@@ -0,0 +1,108 @@
+package com.yoloj.PageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPageObject {
+
+	WebDriver ldriver;
+	
+	public LoginPageObject(WebDriver rdriver){
+		ldriver=rdriver;
+		PageFactory.initElements(rdriver,this);
+	}
+	
+	@FindBy(id="email")
+	WebElement email;
+	
+	@FindBy(id="password")
+	WebElement password;
+	
+	@FindBy(xpath="//*[@id=\"root\"]/div/div/div/div/div/div/div/div[2]/form/button/span[1]")
+	WebElement loginButton;
+	
+	@FindBy(className="MuiTypography-root")
+	WebElement signUplnk;
+	
+	@FindBy(tagName="img")
+	WebElement homePagelnk;
+	
+	@FindBy(xpath="/html/body/div[2]/div[3]/div/ul/li[2]/button/span[1]")
+	WebElement loginBtnLink;
+	
+	@FindBy(xpath="//*[@id=\"root\"]/div/div/div/header/div/div[2]/ul/li[3]/button/span[1]")
+	WebElement registerBtnLink;
+	
+	@FindBy(xpath="//*[@id=\"root\"]/div/div/div/header/div/div[2]/ul/li[1]/div/div/button/span[1]")
+	WebElement providerButton;
+	
+	@FindBy(xpath="//*[@id=\"menu-list\"]/ul/li[2]/a")
+	WebElement providerLoginbtn;
+	
+	@FindBy(xpath="//*[@id=\"menu-list\"]/ul/li[1]/a")
+	WebElement providerRegisterbtn;
+	
+	@FindBy(id="email-helper-text")
+	WebElement emailErrorMsg;
+	
+	@FindBy(id="password-helper-text")
+	WebElement passwordErrorMsg;
+	
+	@FindBy(xpath="/div/div/div[1]")
+	WebElement errorMsgForLogin;
+	
+	public void setEmail(String Email) {
+		email.sendKeys(Email);
+	}
+	
+	public void setPassword(String Password) {
+		password.sendKeys(Password);
+	}
+	
+	public void clickButton() {
+		loginButton.click();
+	}
+
+	public void clickSignUp() {
+		signUplnk.click();
+	}
+	
+	public void clickHomePage() {
+		homePagelnk.click();
+	}
+
+	public String getEmailErrorMsg() {
+		return emailErrorMsg.getText();
+	}
+	
+	public String getPasswordErrorMsg() {
+		return passwordErrorMsg.getText();
+	}
+	
+	public void clickProviderButton() {
+		providerButton.click();
+	}
+	
+	public void clickProviderLoginButton() {
+		providerLoginbtn.click();
+	}
+	
+	public void clickProviderRegisterButton() {
+		providerRegisterbtn.click();
+	}
+	
+	public void clickLoginLink() {
+		loginBtnLink.click();
+	}
+	
+	public void clickRegisterLink() {
+		registerBtnLink.click();
+	}
+	
+	public String getErrorMsg() {
+		return errorMsgForLogin.getText();
+	}
+}
+	
